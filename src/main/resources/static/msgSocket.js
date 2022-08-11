abf87d81be0ceb37835a12c3791b518d9b3f9a95
FIGO <faragmikhail29@gmail.com>
2022-08-11 23:25:52 +0200

    11-8-22 23:25

--- src/main/resources/static/msgSocket.js
+++ src/main/resources/static/msgSocket.js
@@ -1,10 +1,9 @@
-const JSON2 = require("./sockjs");
-let socket = new WebSocket("wss://localhost:8080/test/websocket");
+
+let socket = new WebSocket("ws://localhost:8080/test/websocket");
 
 socket.onopen = function(e) {
-    alert("[open] Connection established");
-    alert("Sending to server");
-    socket.send("My name is John");
+
+
 };
 
 socket.onmessage = function(event) {
@@ -12,16 +11,11 @@ socket.onmessage = function(event) {
 };
 
 socket.onclose = function(event) {
-    if (event.wasClean) {
-        alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-    } else {
-        // e.g. server process killed or network down
-        // event.code is usually 1006 in this case
-        alert('[close] Connection died');
-    }
+
 };
 
 socket.onerror = function(error) {
-    alert(`[error] ${error.message}`);
+
+
 };
 
